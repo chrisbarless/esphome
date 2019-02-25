c6416406d91ed408f9a1b5ef866e5d4c3dac7930
Chris Barless <chris@barless.co>
2019-02-25 03:26:41 -0500

    Improvememnts

--- esphome/sensors/moisture.h
+++ esphome/sensors/moisture.h
@@ -5,7 +5,7 @@
 
 // static const char *TAG = "moisture";
 
-int probePin = 25;
+int vRefPin = 25;
 int sensorPin = 34;
 
 using namespace esphomelib;
@@ -13,19 +13,23 @@ using namespace esphomelib;
 class MoistureSensor : public PollingComponent, public sensor::Sensor {
  public:
 
-  MoistureSensor() : PollingComponent(4000) { }
+  MoistureSensor() : PollingComponent(1800000) { }
 
   void setup() override {
     pinMode(sensorPin, ANALOG);
-    pinMode(probePin, OUTPUT);
-    // digitalWrite(probePin, LOW);
+    pinMode(vRefPin, OUTPUT);
+    digitalWrite(vRefPin, LOW);
   }
 
   void update() override {
+    digitalWrite(vRefPin, HIGH);
+    delay(1000);
     adc1_config_width(ADC_WIDTH_BIT_10);
     adc1_config_channel_atten(ADC1_CHANNEL_6,ADC_ATTEN_DB_11);
-    delay(1000);
     int sensorValue = adc1_get_raw(ADC1_CHANNEL_6);
-    publish_state(sensorValue);
+     publish_state(sensorValue);
+    // publish_state((sensorValue / 1023) * 100);
+    delay(1000);
+    digitalWrite(vRefPin, LOW);
   }
 };
